2fea7d2b22d994d15e04da936e6f3eca9d88ca49
sokomin <sokoranominsyu@gmail.com>
2021-11-21 22:59:27 +0900

    これで自動commitまではされるはず

--- exec.bat
+++ exec.bat
@@ -1,7 +1,13 @@
 @echo off
-python ranking_lv_jpn.py
-python ranking_gh_jpn.py
+git pull origin main
+
+rem "python ranking_lv_jpn.py"
+rem "python ranking_gh_jpn.py"
 
 git add out/jpn_lv/*.csv
+git add out/jpn_gh/*.csv
 git commit -m "test commit"
-git push origin main
+
+rem "pushだけはすると事故る"
+rem "git push origin main"
+
