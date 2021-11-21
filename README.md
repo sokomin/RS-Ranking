5fe13d9dc74ca03e385bf1c2de8fc6334be61fd9
sokomin <sokoranominsyu@gmail.com>
2021-11-22 07:27:46 +0900

    韓国レベルランキング忘れてた

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 
 ## 動かし方
 放置しとけばGitHubActionが1日2回取得しにいってくれます。
+
+
+## トラブルシュート
+- auto commitで事故った時は
+  - `git log`でcommit番号調べる
+  - `git reset <コミットID>`
+  - リバートだと打消しcommitにしかならず、rebaseだと運用が面倒
---
+++ ranking_lv_korea.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+import datetime
+import os
+
+now = datetime.datetime.now()
+today_str = now.strftime("%Y%m%d")
+Path('out/korea_lv/').mkdir(parents=True, exist_ok=True)
+
+# 戻れるのは1日分だけ
+for sn in range(0,4):
+    url = 'http://redstone.logickorea.co.kr/userboard/rankboard/default.aspx?boardType=5&server=' +str(sn)
+    response = requests.get(url)
+    response.raise_for_status()
+    html_ranking = response.text
+
+    mat = []  # 保存先の行列
+    soup = BeautifulSoup(html_ranking, 'html.parser')
+    table = soup.find('table')
+
+    # theadの解析
+    r = [] 
+    thead = table.find('thead') 
+    ths = thead.tr.find_all('th')
+    for th in ths: 
+        r.append(th.text)
+
+    mat.append(r) 
+
+    # tbodyの解析
+    tbody = table.find('tbody')
+    trs = tbody.find_all('tr')
+    for tr in trs:
+        r = []
+        for td in tr.find_all('td'): 
+            r.append(td.text) 
+        mat.append(r)
+
+    # 出力
+    # for r in mat:
+    #     print(','.join(r))  # カンマ（,）で列を結合して表示
+
+    t_server = str(today_str) + '_' + str(sn)
+
+    with open('out/korea_lv/' + t_server + ".csv", "w", encoding='utf-8') as f:
+        for r in mat:
+            f.write(','.join(r))
+            f.write('\n')
+
+    # ファイル量多くなるとGitHubから怒られるので1年以上前のファイルは削除
+    # gitのcommitlog辿れば過去のデータ手に入るしいいよね理論
+    last_year_str = str((now + datetime.timedelta(days=-367)).strftime("%Y%m%d"))
+    tl_server = str(last_year_str) + '_' + str(sn)
+    last_year_file = 'out/korea_lv/'  + tl_server + ".csv"
+    print("delete:" + str(last_year_file))
+    if os.path.exists(last_year_file):
+        os.remove(last_year_file)
