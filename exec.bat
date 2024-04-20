c63f9cee683d2b4c589e025fc261ccff42df3d53
sokomin <sokoranominsyu@gmail.com>
2024-04-20 21:34:44 +0900

    バッチ更新

--- exec.bat
+++ exec.bat
@@ -5,7 +5,8 @@ If not exist log mkdir log
 
 
 git add out/jpn_lv/*.csv >> log/exec.log
-git add out/jpn_gh/*.csv >> log/exec.log
+git add out/jpn_gh_2024/*.csv >> log/exec.log
+git add out/jpn_detective/*.csv >> log/exec.log
 git commit -m "[auto commit]daily japan ranking updated."
 
 rem "pushだけはすると事故る"
